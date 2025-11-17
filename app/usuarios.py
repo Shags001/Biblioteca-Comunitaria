6536646994e14d13a630447e704988cb43ef5a42
Shags001 <dr6496753@gmail.com>
2025-11-17 03:15:38 -0600

    feat: Añadir usuarios.py, CSS de index, JS y recursos de imagen
    - Añadir app/usuarios.py
    - Añadir app/static/css/index.css
    - Añadir app/static/js/main.js
    - Añadir carpeta de imágenes app/static/img/

---
+++ app/usuarios.py
@@ -0,0 +1,6 @@
+usuarios = {
+    "admin@gmail.com": {"password": "admin123", "role": "Administrador", "name": "Alfonso"},
+    "alfonso": {"password": "admin123", "role": "Administrador", "name": "Alfonso"},
+    "recepcionista@gmail.com": {"password": "rep123", "role": "Recepcionista", "name": "Ana"},
+    "ana": {"password": "rep123", "role": "Recepcionista", "name": "Ana"},
+}
